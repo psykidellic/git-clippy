685e8be96f0f722e3747a2519d2fd74a11977546
John Conley <john.conley2@hpe.com>
2017-10-05 15:01:07 -0700

    fixed stuff

--- create-commit-graph.py
+++ create-commit-graph.py
@@ -1,10 +1,14 @@
-from heapq import nlargest
+from collections import Counter
 from itertools import combinations, islice
-from operator import itemgetter
 import os
 
 from clize import run
-from pygit2 import Repository, GIT_SORT_TIME
+from pygit2 import (
+    Repository,
+    GIT_SORT_TIME,
+    GIT_STATUS_CURRENT,
+    GIT_STATUS_IGNORED
+)
 import networkx as nx
 
 
@@ -17,49 +21,64 @@ def find_start(repo, branch=None):
     return start
 
 
-def file_assoc_iter(repo, start, end_sha=None):
-    for commit in repo.walk(start, GIT_SORT_TIME):
-        if commit.hex == end_sha:
+def file_assoc_iter(repo, start, end_sha=None, max_commit_size=100):
+    prev = None
+    for cur in repo.walk(start):
+        print(cur.hex)
+        print(cur.message)
+        if cur.hex == end_sha:
             break
 
-        files = [x.name for x in commit.tree]
-        for comb in combinations(files, 2):
-            yield comb
+        if prev is not None:
+            if len(cur.parents) > 1:
+                print("Merge commit: {} parents".format(cur.parents))
+                print("Merge commit message: {}".format(cur.message))
+            else:
+                diff = cur.tree.diff_to_tree(prev.tree)
+                files = {p.delta.new_file.path for p in diff}
+                if len(files) > max_commit_size:
+                    print("Skipping large commit with {} files"
+                          .format(len(files)))
+                else:
+                    print(files)
+                    for comb in combinations(files, 2):
+                        yield comb
+
+        if cur.parents:
+            prev, cur = cur, cur.parents[0]
 
 
-def update_graph(assoc_iter, g=None):
-    g = g or nx.Graph()
+def increment_edge(g, f1, f2):
+    if g.has_edge(f1, f2):
+        g[f1][f2]['weight'] += 1
+    else:
+        g.add_weighted_edges_from([(f1, f2, 1)])
+
+
+def update_graph(assoc_iter, graph):
     for f1, f2 in assoc_iter:
-        if g.has_edge(f1, f2):
-            g[f1][f2]['weight'] += 1
-        else:
-            g.add_weighted_edges_from([(f1, f2, 1)])
+        increment_edge(graph, f1, f2)
 
-    return g
+    return graph
 
 
-# def recommend(commit, graph, amt=5):
-#
-#     recommended = nlargest(
-#         amt,
-#         ((graph.get_edge_data(f1, f2, {'weight': 0})['weight'], (f1, f2))
-#          for f1, f2 in combinations(commit.stats.files.keys(), 2)),
-#         key=itemgetter[0]
-#     )
-#
-#     edges = [
-#         {'weight': graph[fn_1][fn_2]['weight'], 'file': fn_2} \
-#         for fn_1 in commit.stats.files for fn_2 in graph[fn_1]]
-#     edges.sort(key=lambda x: x['weight'], reverse=True)
-#     if len(edges) > amt:
-#         return [e['file'] for e in edges[:amt]]
-#     else:
-#         return [e['file'] for e in edges]
+def recommend(graph, files, amt):
+    file_counts = Counter()
+    for f1 in enumerate(files):
+        for _, f, data in graph.edges_iter([f1], data=True):
+            file_counts[f] += data['weight']
+
+    return file_counts.most_common(amt)
 
 
-def main(repo_path, *, branch='master', end_sha="", limit=-1):
-    if limit <= 0:
-        limit = None
+def main(
+        repo_path, *,
+        branch='master',
+        end_sha="",
+        num_recommended=10,
+        commit_limit=-1):
+    if commit_limit <= 0:
+        commit_limit = None
 
     graph_filename = os.path.join(repo_path, '.git', 'file_assoc_graph.pkl')
     sha_filename = os.path.join(repo_path, '.git', 'end_sha.txt')
@@ -71,13 +90,25 @@ def main(repo_path, *, branch='master', end_sha="", limit=-1):
         except IOError:
             pass
 
+    try:
+        graph = nx.read_gpickle(graph_filename)
+    except IOError:
+        graph = nx.Graph()
+
     repo = Repository(repo_path)
     start = find_start(repo, branch)
-    assoc_iter = islice(file_assoc_iter(repo, start, end_sha), limit)
-    graph = update_graph(assoc_iter)
+    assoc_iter = islice(file_assoc_iter(repo, start, end_sha), commit_limit)
+    graph = update_graph(assoc_iter, graph)
     nx.write_gpickle(graph, graph_filename)
     with open(sha_filename, 'w') as sha_file:
         sha_file.write(start.hex)
+
+    changed_files = [
+        filename for filename, status in repo.status().items()
+        if status not in (GIT_STATUS_CURRENT, GIT_STATUS_IGNORED)
+    ]
+    for item, count in recommend(graph, changed_files, num_recommended):
+        print("{} with co-occurence {}".format(item, count))
 
 
 if __name__ == "__main__":
