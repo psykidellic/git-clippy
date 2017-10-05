22d211b920284e6cf943c49fdc1afc27b91b3a5d
C. Dylan Shearer <charles.shearer@hpe.com>
2017-10-05 11:55:47 -0700

    Bugfix: sorting in reverse order now

--- recommend.py
+++ recommend.py
@@ -1,10 +1,10 @@
 import networkx as nx
 
-def recommend(commit, graph):
+def recommend(commit, graph, amt=5):
     edges = [{'weight': graph[fn_1][fn_2]['weight'], 'file': fn_2} \
         for fn_1 in commit.stats.files for fn_2 in graph[fn_1]]
-    edges.sort(key=lambda x: x['weight'])
-    if len(edges) > 5:
-        return [e['file'] for e in edges[:5]]
+    edges.sort(key=lambda x: x['weight'], reverse=True)
+    if len(edges) > amt:
+        return [e['file'] for e in edges[:amt]]
     else:
         return [e['file'] for e in edges]
